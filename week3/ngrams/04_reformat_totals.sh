58dd5540d334e866afffbd8315eddff9b3bf5126
DereckTav <derecktaverne@gmail.com>
2025-06-16 10:44:40 -0400

    ngrams

--- week3/ngrams/04_reformat_totals.sh
+++ week3/ngrams/04_reformat_totals.sh
@@ -3,3 +3,5 @@
 # reformat total counts in googlebooks-eng-all-totalcounts-20120701.txt to a valid csv
 #   use tr, awk, or sed to convert tabs to newlines
 #   write results to total_counts.csv
+
+awk -F '\t' '{for (i=2; i <= NF; i++) { print $i }}' googlebooks-eng-all-totalcounts-20120701.txt > total_counts.csv
